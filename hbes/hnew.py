983e9323c0f966336467947591a4b5c609d3bc20
Leizuohong <lzh791397845@gmail.com>
2020-07-13 13:34:42 +0800

    123

--- hbes/hnew.py
+++ hbes/hnew.py
@@ -20,7 +20,8 @@ class SELECTER:
                  text='',
                  show=0,
                  inter=100,
-                 title=''):
+                 title='',
+                 reverse=0):
         '初始化'
         # 判断类变量
         self.left = center - width
@@ -32,6 +33,7 @@ class SELECTER:
         self.show = show
         self.inter = inter
         self.title = title
+        self.reverse = reverse
 
     # 方法：判断
     def judge(self, input):
@@ -41,6 +43,8 @@ class SELECTER:
             a = input >= self.left
             b = input <= self.right
             output = a * b
+        if(self.reverse != 0):
+            output = 1 - output
         return output
 
     # 方法：改变变量
@@ -74,6 +78,9 @@ class SELECTER:
         self.right = self.right + value
         self.center = self.center + value
 
+    def reverse(self):
+        self.reverse = 1 - self.reverse
+
     # 方法：获取信息
     def get_range(self):
         return self.left, self.right
@@ -99,6 +106,19 @@ class SELECTER:
         output['width'] = self.width
         output['text'] = self.text
         return output
+
+    # 特殊方法
+    def set_series(self, series):
+        self.series = series
+
+    def judge_by_series(self, input):
+        output = False
+        if(input in self.series()):
+            output = True
+        if(self.reverse != 0):
+            output = 1 - output
+        return output
+
 
 # root 读取类
 
@@ -297,6 +317,97 @@ def tree_cut(dict_tree,
         dict_tree_new[i] = dict_tree[i][list_bool]
     return dict_tree_new
 
+
+def bin_search(data, l, r, i, d):
+    '''
+    data: 数值\n
+    l, r: 左边界和右边界\n
+    i: 总bin数\n
+    d: 分bin间隔\n
+    \n
+    1：数组数值，输入坐标轴信息，返回所在bin\n
+    '''
+    output = int((data - l) / d)
+    if(output > i - 1 or output < 0):
+        output = 'empty'
+    return output
+
+
+def tree_addweight2d(in_tree={},
+                     weight={},
+                     name_branch='',
+                     name_ratio=''):
+    '''
+    in_tree: tree\n
+    weight: weight信息字典\n
+    name_branch: tree中存放权重的branch名\n
+    name_ratio: weight中存放权重矩阵的key名\n
+    \n
+    作用：输入weight信息字典，给tree添加权重的branch\n
+    '''
+    # 新tree初始化
+    out_tree = in_tree
+    num = 0
+    for i in in_tree:
+        num = len(in_tree[i])
+    out_tree[name_branch] = numpy.array([])
+    # 填入weight
+    for i in range(num):
+        bx = bin_search(in_tree[weight['branchx']][i],
+                        weight['xl'],
+                        weight['xr'],
+                        weight['xi'],
+                        weight['dx'])
+        by = bin_search(in_tree[weight['branchy']][i],
+                        weight['yl'],
+                        weight['yr'],
+                        weight['yi'],
+                        weight['dy'])
+        if(bx != 'empty' and by != 'empty'):
+            out_tree[name_branch] = numpy.append(out_tree[name_branch], weight[name_ratio][bx][by])
+        else:
+            out_tree[name_branch] = numpy.append(out_tree[name_branch], 0)
+    return out_tree
+
+
+def tree_addweight1d(in_tree={},
+                     weight={},
+                     name_branch='',
+                     name_ratio=''):
+    '''
+    in_tree: tree\n
+    weight: weight信息字典\n
+    name_branch: tree中存放权重的branch名\n
+    name_ratio: weight中存放权重矩阵的key名\n
+    \n
+    作用：输入weight信息字典，给tree添加权重的branch\n
+    '''
+    # 新tree初始化
+    out_tree = in_tree
+    num = 0
+    for i in in_tree:
+        num = len(in_tree[i])
+    out_tree[name_branch] = numpy.array([])
+    # 填入weight
+    for i in range(num):
+        bx = bin_search(in_tree[weight['branchx']][i],
+                        weight['xl'],
+                        weight['xr'],
+                        weight['xi'],
+                        weight['dx'])
+        if(bx != 'empty'):
+            out_tree[name_branch] = numpy.append(out_tree[name_branch], weight[name_ratio][bx])
+        else:
+            out_tree[name_branch] = numpy.append(out_tree[name_branch], 0)
+    return out_tree
+
+
+def tree_len(dict_tree):
+    output = 0
+    for i in dict_tree:
+        output = len(dict_tree[i])
+    return output
+
 # 系统信息处理类
 
 
@@ -411,18 +522,16 @@ class ALLDATA:
         weight_file = '%s/%s_%1.4f.pkl' % (self.massages['weight'],
                                            name_weight,
                                            energy)
-        with open(weight_file, 'rb') as infile:
-            weight = pickle.load(infile)
+        weight = hpickle.pkl_read(weight_file)
         # 更改cut
-        self.cuts[weight['branchx']]['mass'] = 0.5 * (weight['xr'] + weight['xl'])
-        self.cuts[weight['branchx']]['range'] = 0.5 * (weight['xr'] - weight['xl'])
-        self.cuts[weight['branchx']]['inter'] = weight['xi']
+        self.selections[weight['branchx']].set_by_edge(weight['xl'], weight['xr'])
+        self.selections[weight['branchx']].set_inter(weight['xi'])
         # 添加tree[name]的weight数组branch
         self.weights.append(name_branch)
-        self.trees[data] = htree.tree_addweight1d(self.trees[data],
-                                                  weight,
-                                                  name_branch=name_branch,
-                                                  name_ratio=name_ratio)
+        self.trees[data] = tree_addweight1d(self.trees[data],
+                                            weight,
+                                            name_branch=name_branch,
+                                            name_ratio=name_ratio)
 
     def get_weight(self,
                    data='',
@@ -432,15 +541,16 @@ class ALLDATA:
                    name_ratio='',
                    dimension=2):
         '''
-        data为字符，输入要进行weighting的tree的name\n
-        energy为浮点，输入要进行的weighting的能量点\n
-        name为字符，输入要进行weighting的method的名字\n
-        dimension为整形，输入要进行weighting的维度\n
-        ratio_name为字符，输入weighting数据中心代表要使用的阵列的key\n
-        作用1：给tree添加name的branch，数值为权重\n
-        作用2：将name添加进内部变量weights\n
-        Note:\n
-        weight信息请放置在massage['weight']/name_energy.pkl多的文件中\n
+        data: 进行weighting的tree的name\n
+        energy: 进行weighting的能量点\n
+        name_weight: 进行weighting的method的名字\n
+        name_branch: 添加的branch的名字\n
+        name_ratio: weighting阵列的key\n
+        dimension: 进行weighting的维度\n
+        \n
+        1：给tree添加name_branch的branch，数值为权重\n
+        \n
+        Note: weight信息请放置在 massage['weight']/name_energy.pkl 的文件中\n
         '''
         if(dimension == 2):
             self.get_weight_2d(data=data,
@@ -456,3 +566,146 @@ class ALLDATA:
                                name_ratio=name_ratio)
         else:
             print('error：维度输入错误')
+
+    def hist1d(self,
+               data='',
+               branch='',
+               docuts=[],
+               doweight=[],
+               name=''):
+        '内部调用函数'
+        # 输出信息
+        hprint.pline('Building TH1D')
+        hprint.ppoint('Source', data)
+        hprint.ppoint('Branch', branch)
+        # 得到cut后的tree
+        ntree = tree_cut(self.trees[data], self.selecters, docuts)
+        # 新建root文件,hist对象
+        tfilename = 'ftemp/root/%s_%s_%s.root' % (data, branch, name)
+        histname = '%s_%s' % (data, branch)
+        inter = self.selecters[branch].inter
+        left, right = self.selecters[branch].get_range()
+        num = len(ntree[branch])
+        # 开始填入直方图
+        if (len(doweight) == 0):
+            hprint.ppoint('Weight', 'NO')
+            tfilename, histname = hist1d(name_tfile=tfilename,
+                                         name_hist=histname,
+                                         inter=inter,
+                                         left=left,
+                                         right=right,
+                                         data=ntree[branch])
+        else:
+            hprint.ppoint('Weight', 'YES')
+            weight = []
+            for i in range(num):
+                factor = 1
+                for name in doweight:
+                    factor = factor * ntree[name][i]
+                weight.append(factor)
+            tfilename, histname = hist1d(name_tfile=tfilename,
+                                         name_hist=histname,
+                                         inter=inter,
+                                         left=left,
+                                         right=right,
+                                         data=ntree[branch],
+                                         weight=weight)
+        hprint.pstar()
+        return tfilename, histname
+
+    def hist2d(self,
+               data='',
+               branch1='',
+               branch2='',
+               docuts=[],
+               doweight=[],
+               name=''):
+        '内部调用函数'
+        # 输出信息
+        hprint.pline('Building TH1D')
+        hprint.ppoint('Source', data)
+        hprint.ppoint('Branch1', branch1)
+        hprint.ppoint('Branch2', branch2)
+        # 得到cut后的tree
+        ntree = tree_cut(self.trees[data], self.selecters, docuts)
+        # 新建root文件,hist对象
+        tfilename = 'ftemp/root/%s_%s_%s_%s.root' % (data,
+                                                     branch1,
+                                                     branch2,
+                                                     name)
+        histname = '%s_%s_%s' % (data,
+                                 branch1,
+                                 branch2)
+        inter1 = self.selecters[branch1].inter
+        left1, right1 = self.selecters[branch1].get_range()
+        inter2 = self.selecters[branch2].inter
+        left2, right2 = self.selecters[branch2].get_range()
+        data1 = ntree[branch1]
+        data2 = ntree[branch2]
+        num = len(data1)
+        # 开始填入直方图
+        if (len(doweight) == 0):
+            hprint.ppoint('Weight', 'NO')
+            tfilename, histname = hist2d(name_tfile=tfilename,
+                                               name_hist=histname,
+                                               inter1=inter1,
+                                               left1=left1,
+                                               right1=right1,
+                                               inter2=inter2,
+                                               left2=left2,
+                                               right2=right2,
+                                               data1=data1,
+                                               data2=data2)
+        else:
+            hprint.ppoint('Weight', 'YES')
+            weight = []
+            for i in range(num):
+                factor = 1
+                for name in self.weights:
+                    factor = factor * ntree[name][i]
+                weight.append(factor)
+            tfilename, histname = hist2d(name_tfile=tfilename,
+                                               name_hist=histname,
+                                               inter1=inter1,
+                                               left1=left1,
+                                               right1=right1,
+                                               inter2=inter2,
+                                               left2=left2,
+                                               right2=right2,
+                                               data1=data1,
+                                               data2=data2,
+                                               weight=weight)
+        hprint.pstar()
+        return tfilename, histname
+
+    def hist(self,
+             data='',
+             branchs=[],
+             docuts=[],
+             doweight=[],
+             name=''):
+        '''
+        data: 数据来源的tree\n
+        branchs: 提取的数据的branch名\n
+        docuts: 进行筛选的branch名\n
+        doweight: 按列表进行weighting\n
+        name为字符，提供文件名保证名称不重复\n
+        \n
+        1：返回tfile的名字，thist的名字]\n
+        \n
+        Note: 请在目录ftemp文件夹下建立root文件夹
+        '''
+        if(len(branchs) == 1):
+            tfilename, histname = self.hist1d(data,
+                                              branchs[0],
+                                              docuts,
+                                              doweight,
+                                              name)
+        if(len(branchs) == 2):
+            tfilename, histname = self.hist2d(data,
+                                              branchs[0],
+                                              branchs[1],
+                                              docuts,
+                                              doweight,
+                                              name)
+        return tfilename, histname
